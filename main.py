e608f510f062e6bf4c2c53b8b29b0c892d55c0e1
Gregory <gregory.leblond.dev@gmail.com>
2024-06-12 18:48:45 +0200

    feat: add llm interaction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
-
 from fastapi import FastAPI
+from Prompt import Prompt
+from mistral import generate_text_with_mistral
 
 app = FastAPI()
 
@@ -8,6 +9,15 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/llm")
+def llm(prompt: Prompt):
+    if prompt.message:
+        response = generate_text_with_mistral(prompt.message)
+        return {"message": response}
+    else:
+        return {"error": "Le prompt est null"}
 
 
 @app.get("/items/{item_id}")
---
+++ mistral.py
@@ -0,0 +1,29 @@
+from mistral_inference.model import Transformer
+from mistral_inference.generate import generate
+from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
+from mistral_common.protocol.instruct.messages import UserMessage, SystemMessage
+from mistral_common.protocol.instruct.request import ChatCompletionRequest
+from pathlib import Path
+from Prompt import Prompt
+
+
+def generate_text_with_mistral(prompt: Prompt):
+    mistral_models_path = Path.home().joinpath("Mistral-7B-Instruct-v0.3")
+
+    tokenizer = MistralTokenizer.from_file(
+        f"{mistral_models_path}/tokenizer.model.v3")
+
+    model = Transformer.from_folder(mistral_models_path)
+
+    completion_request = ChatCompletionRequest(messages=[SystemMessage(
+        content="You are a pirate chatbot who always responds in pirate speak!"), UserMessage(content=prompt.message), ])
+
+    tokens = tokenizer.encode_chat_completion(completion_request).tokens
+
+    out_tokens, _ = generate([tokens], model, max_tokens=256, temperature=0.0,
+                             eos_id=tokenizer.instruct_tokenizer.tokenizer.eos_id)
+
+    result = tokenizer.instruct_tokenizer.tokenizer.decode(out_tokens[0])
+
+    print(result)
+    return result
